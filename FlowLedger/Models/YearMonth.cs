cd809f2e3a76ec930ada84a56075d50b39a9b67c
LJ_Tao <lingjie.tao63@gmail.com>
2025-06-26 15:27:32 +0200

    init - to include Year for the keys of monthlytransactions

---
+++ FlowLedger/Models/YearMonth.cs
@@ -0,0 +1,12 @@
+﻿namespace FlowLedger.Models
+{
+    internal struct YearMonth : IEquatable<YearMonth>
+    {
+        public int Year { get; }
+        public int Month { get; }
+        public bool Equals(YearMonth other)
+        {
+            return Year == other.Year && Month == other.Month;
+        }
+    }
+}
