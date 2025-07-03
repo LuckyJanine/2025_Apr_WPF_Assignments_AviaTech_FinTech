8f608cc81b8e129bd5b8ac876c33871b29d1ea5b
LJ_Tao <lingjie.tao63@gmail.com>
2025-07-03 17:36:40 +0200

    ctor for YearModel struct

--- FlowLedger/Models/YearMonth.cs
+++ FlowLedger/Models/YearMonth.cs
@@ -4,6 +4,12 @@
     {
         public int Year { get; }
         public int Month { get; }
+
+        public YearMonth(int year, int month)
+        {
+            Year = year;
+            Month = month;
+        }
         public bool Equals(YearMonth other)
         {
             return Year == other.Year && Month == other.Month;
